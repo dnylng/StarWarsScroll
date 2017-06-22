262729d38ff84101d1b6745197a932b9cf657cb9
dnylng <iamdannyluong@gmail.com>
2017-06-22 17:29:28 -0400

    Added the scroll view with icons
    I added the scroll views attached to an array of icons in the assets folder. The app successfully scrolls through characters.

--- StarWarsScroll/ViewController.swift
+++ StarWarsScroll/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Reference to the scroll view
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // An array of images to display and scroll through
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var contentWidth: CGFloat = 0.0 // Init a var that determines scrolling content width
+        
+        // Populate the images array
+        for i in 0...14 {
+            let image = UIImage(named: "Individual-\(i).png") // Create an image taken from our assets
+            let imageView = UIImageView(image: image) // Create an image view out of previous image
+            images.append(imageView) // Add the image view into the array
+            
+            var newX: CGFloat = 0.0 // Init a new variable to calc position of images when scrolling
+            newX = view.frame.midX + view.frame.size.width * CGFloat(i) // Start in middle, add frame width depending on the index
+            contentWidth += view.frame.size.width // Everytime we add a new image, increase content width
+            
+            scrollView.addSubview(imageView) // Add the image view under the scroll view
+            
+            imageView.frame = CGRect(x: newX - 75, y: view.frame.midY - 150, width: 150, height: 150) // Center the image
+        }
+        
+        // Checking the size of the array there should be 15
+        print("Images Count: \(images.count)")
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+        // Checking the size of the content should be 15 * frame
+        print("Frame Width: \(view.frame.size.width)")
+        print("Estimated Content Width: \(view.frame.size.width * 15)")
+        print("Actual Content Width: \(contentWidth)")
     }
 
     override func didReceiveMemoryWarning() {
